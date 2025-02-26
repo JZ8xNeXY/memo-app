4d969a3664669d76219d442c39592988bcb0f7f7
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-02-26 21:22:10 +0900

    index.tsxの修正

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,47 +1,53 @@
-import { View,Text,StyleSheet } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
-const Index = ():JSX.Element => {
+const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      {/* ヘッダー */}
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
+      {/* メモリスト */}
       <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年10月1日10:00</Text>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日10:00</Text>
+          </View>
+          <View>
+            <Text>✖️</Text>
+          </View>
         </View>
-        <View>
-          <Text>✖️</Text>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日10:00</Text>
+          </View>
+          <View>
+            <Text>✖️</Text>
+          </View>
         </View>
-      </View>
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年10月1日10:00</Text>
-        </View>
-        <View>
-          <Text>✖️</Text>
-        </View>
-      </View><View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年10月1日10:00</Text>
-        </View>
-        <View>
-          <Text>✖️</Text>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日10:00</Text>
+          </View>
+          <View>
+            <Text>✖️</Text>
+          </View>
         </View>
       </View>
 
-      <View>
-        <Text>+</Text>
+      {/* 追加ボタン */}
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
-    
     </View>
   )
 }
@@ -51,7 +57,70 @@ export default Index
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center' 
+    backgroundColor: "#ffffff"
+  },
+  header: {
+    width: "100%",
+    height: 104,
+    backgroundColor: "#467FD3",
+    justifyContent: "flex-end"
+  },
+  headerInner: {
+    alignItems: "center"
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff"
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255,255,255,0.7)"
+  },
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.15)"
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484"
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    backgroundColor: "#467FD3",
+    borderRadius: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4.65,
+    elevation: 6
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 40
   }
 })
