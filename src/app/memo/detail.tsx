34bdd622c159f40bfc8440062a9cd4d0924ced5d
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-03-02 19:33:30 +0900

    編集機能の作成

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -7,20 +7,20 @@ import {db,auth} from '../../config'
 import { type Memo } from "../../../types/memo"
 import { useEffect,useState } from "react"
 
-const handlePress = ():void =>{
- router.push('/memo/edit')
+const handlePress = (id:string):void =>{
+ router.push({pathname:'/memo/edit',params:{id}})
 }
 
 const Detail = ():JSX.Element => {
-  const {id} = useLocalSearchParams()
-  console.log(id)
+  const id = String(useLocalSearchParams().id)
+
 
   const [memo,setMemo] = useState<Memo|null>(null)
 
   useEffect(() => {
     if (auth.currentUser === null ) {return }
     
-    const ref = doc(db,`users/${auth.currentUser.uid}/memos`,String(id))
+    const ref = doc(db,`users/${auth.currentUser.uid}/memos`,id)
     const unsubscribe = onSnapshot(ref,(memoDoc) => {
       const {bodyText,updatedAt} = memoDoc.data() as Memo
       setMemo({
@@ -43,8 +43,8 @@ const Detail = ():JSX.Element => {
         {memo?.bodyText}  
         </Text>
       </ScrollView>
-      <CircleButton onPress={handlePress} style={{top:60,bottom:'auto'}}>
-      <Icon  name='pencil' size={40} color='#ffffff' />
+      <CircleButton onPress={() => {handlePress(id)}} style={{top:60,bottom:'auto'}}>
+        <Icon  name='pencil' size={40} color='#ffffff' />
       </CircleButton>
     </View>
     
@@ -80,9 +80,9 @@ const styles = StyleSheet.create({
     paddingHorizontal:27
   },
   memoBodyText:{
+    paddingVertical:32,
     fontSize:16,
     lineHeight:24,
-    color:'#000',
-    paddingVertical:32
+    color:'#000'
   }
 })
